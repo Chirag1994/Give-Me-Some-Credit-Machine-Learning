fe80b00a8cca77292272009d8915b163a0c939cb
Chirag Sharma <chirag.sharma0378@gmail.com>
2023-04-06 11:13:18 +0530

    log file modified

--- Give_Me_Some_Credit/logger/__init__.py
+++ Give_Me_Some_Credit/logger/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import pandas as pd
 from datetime import datetime
 
 LOG_DIR = "logs"
@@ -14,5 +15,27 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     filemode="w",
     level=logging.INFO,
-    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s",
 )
+
+
+def get_log_dataframe(file_path):
+    data = []
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns = [
+        "Time stamp",
+        "Log Level",
+        "line number",
+        "file name",
+        "function name",
+        "message",
+    ]
+    log_df.columns = columns
+
+    log_df["log_message"] = log_df["Time stamp"].astype(str) + ":$" + log_df["message"]
+
+    return log_df[["log_message"]]
